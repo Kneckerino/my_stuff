9564a1c214c6f15dde8f05929493c57d3a9dc020
Kneckerino <mrenne22@student.aau.dk>
2025-02-21 15:19:26 +0100

    Now also creates edges from class attributes

--- first_example/src/main.rs
+++ first_example/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::hash_map;
 //Std stuff
 use std::fs::File;
 use std::io::BufReader;
@@ -13,6 +14,7 @@ use petgraph::graph;
 use petgraph::visit::NodeRef;
 use petgraph::Graph;
 use petgraph::dot::Dot;
+use petgraph::graph::NodeIndex;
 
 //Serde
 use serde_json::{Result, Value};
@@ -39,14 +41,14 @@ struct OwlToWovlJSON {
 #[derive(Serialize, Deserialize, Debug)]
 struct Header {
     languages:              Vec<String>,
-    baseIris:               Vec<String>,
-    title:                  Title,
+    baseIris:               Option<Vec<String>>,
+    title:                  Label,
     iri:                    String,
     version:                String,
     author:                 Vec<String>,
-    description:            Description,
-    labels:                 Label,
-    other:                  Other,
+    description:            Label,
+    labels:                 Option<Label>,
+    other:                  Option<Other>,
 }
 #[derive(Serialize, Deserialize, Debug)]
 struct Metrics {
@@ -59,19 +61,30 @@ struct Metrics {
 struct Class {
     id:                     String,
     r#type:                 String,
+    label:                  Option<String>,
+    intersection:           Option<Vec<String>>,
+    union:                  Option<Vec<String>>,
+    disjointUnion:          Option<Vec<String>>,
+    complement:             Option<Vec<String>>,
 }
 #[derive(Serialize, Deserialize, Debug)]
 struct ClassAttribute {
+    id:                     String,
     iri:                    Option<String>,
     baseIri:                Option<String>,
     instances:              Option<u32>,
+    individuals:            Option<Vec<Individuals>>,
     annotations:            Option<Annotations>,
     label:                  Option<Label>,
-    subClasses:             Option<Vec<String>>,
     comment:                Option<Label>,
     attributes:             Option<Vec<String>>,
-    id:                     String,
     superClasses:           Option<Vec<String>>,
+    subClasses:             Option<Vec<String>>,
+    complement:             Option<Vec<String>>,
+    union:                  Option<Vec<String>>,
+    intersection:           Option<Vec<String>>,
+    equivalent:             Option<Vec<String>>,
+    disjointUnion:          Option<Vec<String>>,
 }
 #[derive(Serialize, Deserialize, Debug)]
 struct Property {
@@ -96,36 +109,45 @@ struct PropertyAttribute {
 
 // & Level 2
 #[derive(Serialize, Deserialize, Debug)]
-struct Title {
-    undefined:              String,
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct Description {
-    undefined:              String,
-}
-#[derive(Serialize, Deserialize, Debug)]
 struct Label {
-    // ^ Struct for both labels and comments
+    // ^ Struct for both labels, comments, title and description
     #[serde(rename = "IRI-based")]
     IRI_based:              Option<String>,
+    iriBased:               Option<String>,
     undefined:              Option<String>,
     en:                     Option<String>,
+    de:                     Option<String>,
+    fr:                     Option<String>,
+    es:                     Option<String>,
 }
 #[derive(Serialize, Deserialize, Debug)]
 struct Other {
-    licence:                Vec<ILVT>,
-    creator:                Vec<ILVT>,
-    versionInfo:            Vec<ILVT>,
-    title:                  Vec<ILVT>,
-    issued:                 Vec<ILVT>,
-    seeAlso:                Vec<ILVT>,
-    homepage:               Vec<ILVT>,
-    depiction:              Vec<ILVT>,
+    licence:                Option<Vec<ILVT>>,
+    creator:                Option<Vec<ILVT>>,
+    versionInfo:            Option<Vec<ILVT>>,
+    title:                  Option<Vec<ILVT>>,
+    issued:                 Option<Vec<ILVT>>,
+    seeAlso:                Option<Vec<ILVT>>,
+    homepage:               Option<Vec<ILVT>>,
+    depiction:              Option<Vec<ILVT>>,
+    priorVersion:           Option<Vec<ILVT>>,
+    date:                   Option<Vec<ILVT>>,
+    contributor:            Option<Vec<ILVT>>,
+    incompatibleWith:       Option<Vec<ILVT>>,
+    rights:                 Option<Vec<ILVT>>,
+    backwardCompatibleWith: Option<Vec<ILVT>>,
 }
 #[derive(Serialize, Deserialize, Debug)]
 struct Annotations {
     isDefinedBy:            Option<Vec<ILVT>>,
     versionInfo:            Option<Vec<ILVT>>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Individuals {
+    iri:                    String,
+    baseIri:                String,
+    description:            Option<Label>,
+    labels:                 Option<Label>,
 }
 
 // & Level 3
@@ -141,14 +163,14 @@ struct ILVT {
 fn main() {
 
     // * Read the JSON file */
-    let file = File::open("./src/modified.json").unwrap();
+    let file = File::open("./src/ontovibe.json").unwrap();
     let reader = BufReader::new(file);
     let graph_struct: OwlToWovlJSON = serde_json::from_reader(reader).unwrap();
     
     // * Create the graph */
     let mut graph = Graph::<String, ()>::new();
 
-    //Hashmap is necessary to store the nodes references with their new ids
+    //Hashmap is necessary to store the nodes references with their ids
     let mut hashmap   = HashMap::new();
 
     for edge in &graph_struct.propertyAttribute {
@@ -166,7 +188,7 @@ fn main() {
                 dom_index = x;
             },
             None => {
-                dom_index = graph.add_node(domain_node_id.clone());
+                dom_index = graph.add_node(edge.domain.clone());
                 hashmap.insert(domain_node_id, dom_index);
             }
         }
@@ -177,26 +199,88 @@ fn main() {
                 ran_index = x;
             },
             None => {
-                ran_index = graph.add_node(range_node_id.clone());
+                ran_index = graph.add_node(edge.range.clone());
                 hashmap.insert(range_node_id, ran_index);
             }
         }
         graph.add_edge(dom_index, ran_index, ());
     }
+
+    for node in graph_struct.class {
+        let node_id = node.id.clone();
+        let opt_index = hashmap.get(&node.id);
+        match opt_index {
+            Some(&x) => {
+            },
+            None => {
+                hashmap.insert(node_id, graph.add_node(node.id.clone()));
+            }
+        }
+    }
+
+    for attr in graph_struct.classAttribute {
+        let attr_id = attr.id.clone();
+        let opt_index = hashmap.get(&attr.id);
+
+        let dom_index;
+        match opt_index {
+            Some(&x) => {
+                dom_index = x;
+            },
+            None => {
+                println!("WARNING! Node does not exist, creating node: {:?}", attr.id);
+                dom_index = graph.add_node(attr.id.clone());
+                hashmap.insert(attr_id, dom_index);
+            }
+        }
+        make_edges(dom_index, attr.superClasses.clone(), &mut graph, &mut hashmap);
+        make_edges(dom_index, attr.subClasses.clone(), &mut graph, &mut hashmap);
+        make_edges(dom_index, attr.complement.clone(), &mut graph, &mut hashmap);
+        make_edges(dom_index, attr.union.clone(), &mut graph, &mut hashmap);
+        make_edges(dom_index, attr.intersection.clone(), &mut graph, &mut hashmap);
+        make_edges(dom_index, attr.equivalent.clone(), &mut graph, &mut hashmap);
+        make_edges(dom_index, attr.disjointUnion.clone(), &mut graph, &mut hashmap);
+    }
     
     // ! Debugging stuff
-    println!("{:?} == {:?}", graph_struct.class.len(), graph_struct.metrics.as_ref().unwrap().objectPropertyCount);
-    println!("{:?} == {:?}", graph_struct.propertyAttribute.len(), graph_struct.property.len());
+    println!("{:?} == {:?}", graph_struct.propertyAttribute.len(), 
+    graph_struct.property.len());
     println!("{:?}", Dot::new(&graph));
 
     // * Configure the simulator */
     let simulator = SimulatorBuilder::new()
-        .delta_time(0.01)
+        .delta_time(0.005)
         .freeze_threshold(-1.0)
         .build(graph.into());
 
     // * Start the renderer */
     let renderer = Renderer::new(simulator);
     renderer.create_window();
-    
 }
+
+fn make_edges (domain: NodeIndex, opt_vector: Option<Vec<String>>, graph: &mut Graph<String, ()>, hashmap: &mut HashMap<String, NodeIndex>) {
+    match opt_vector {
+
+        Some(vector) => {
+
+            for id_string in vector {
+                
+                let opt_index = hashmap.get(&id_string);
+                match opt_index {
+
+                    Some(ran_index) => {
+                        graph.add_edge(domain, *ran_index, ());
+                    }
+                    None => {
+                        println!("WARNING! Node does not exist, creating node: {:?}", id_string);
+                        let ran_index = graph.add_node(id_string.clone());
+                        hashmap.insert(id_string, ran_index);
+                        graph.add_edge(domain, ran_index, ());
+                    }
+                }
+            }
+        },
+        None => {}
+    }
+}
+
